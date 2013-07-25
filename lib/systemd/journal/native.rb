a8c80c4a51b9cc0432938b28ff3bd40a5d39928e
John Ledbetter <john.ledbetter@gmail.com>
2013-07-24 20:46:06 -0400

    line up native function declarations

--- lib/systemd/journal/native.rb
+++ lib/systemd/journal/native.rb
@@ -5,9 +5,10 @@ module Systemd
       extend FFI::Library
       ffi_lib %w[libsystemd-journal.so libsystemd-journal.so.0]
 
-      attach_function :sd_journal_open, [:pointer, :int], :int
+      # setup/teardown
+      attach_function :sd_journal_open,           [:pointer, :int], :int
       attach_function :sd_journal_open_directory, [:pointer, :string, :int], :int
-      attach_function :sd_journal_close, [:pointer], :void
+      attach_function :sd_journal_close,          [:pointer], :void
 
       # navigation
       attach_function :sd_journal_next,          [:pointer], :int
@@ -15,23 +16,27 @@ module Systemd
       attach_function :sd_journal_previous,      [:pointer], :int
       attach_function :sd_journal_previous_skip, [:pointer, :uint64], :int
 
-      attach_function :sd_journal_get_data, [:pointer, :string, :pointer, :pointer], :int
-      attach_function :sd_journal_restart_data, [:pointer], :void
-      attach_function :sd_journal_enumerate_data, [:pointer, :pointer, :pointer], :int
-
-      attach_function :sd_journal_seek_head, [:pointer], :int
-      attach_function :sd_journal_seek_tail, [:pointer], :int
+      attach_function :sd_journal_seek_head,          [:pointer], :int
+      attach_function :sd_journal_seek_tail,          [:pointer], :int
       attach_function :sd_journal_seek_realtime_usec, [:pointer, :uint64], :int
 
+      # data reading
+      attach_function :sd_journal_get_data,       [:pointer, :string, :pointer, :pointer], :int
+      attach_function :sd_journal_restart_data,   [:pointer], :void
+      attach_function :sd_journal_enumerate_data, [:pointer, :pointer, :pointer], :int
+
+      # event notification
       attach_function :sd_journal_wait, [:pointer, :uint64], :int
 
-      attach_function :sd_journal_add_match, [:pointer, :string, :size_t], :int
-      attach_function :sd_journal_flush_matches, [:pointer], :void
+      # filtering
+      attach_function :sd_journal_add_match,       [:pointer, :string, :size_t], :int
+      attach_function :sd_journal_flush_matches,   [:pointer], :void
       attach_function :sd_journal_add_disjunction, [:pointer], :int
       attach_function :sd_journal_add_conjunction, [:pointer], :int
 
+      # writing
       attach_function :sd_journal_print, [:int, :string], :int
-      attach_function :sd_journal_send, [:varargs], :int
+      attach_function :sd_journal_send,  [:varargs], :int
     end
 
   end
