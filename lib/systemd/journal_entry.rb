60ced7635baa0394f30988aff78d05c21746a79d
John Ledbetter <john.ledbetter@gmail.com>
2014-07-08 19:04:46 -0400

    add some missing documentation.

--- lib/systemd/journal_entry.rb
+++ lib/systemd/journal_entry.rb
@@ -37,6 +37,7 @@ module Systemd
       @monotonic_timestamp ||= Time.at(0, @ctx[:monotonic_ts].first)
     end
 
+    # @private
     def method_missing(m, *args)
       # not all journal entries will have all fields.  don't raise an error
       # unless the user passed arguments.
@@ -44,15 +45,27 @@ module Systemd
     end
 
     # Get the value of a given field in the entry, or nil if it doesn't exist
+    # @param [String] the field name for which to look up the value. Can be a
+    #   symbol or string, case insensitive.
+    # @return [String] the value for the given field, or nil if not found.
     def [](key)
       @entry[key] || @entry[key.to_s.upcase]
     end
 
+    # Yields each field name and value pair to the provided block.
+    # If no block is given, returns an enumerator.
+    # @return [Enumerator]
     def each
       return to_enum(:each) unless block_given?
       @entry.each { |key, value| yield [key, value] }
     end
 
+    # Returns the catalog message that this Journal Entry references, if any.
+    # @option opts [Boolean] :replace set to false to not replace placeholder
+    #   strings in the catalog with the associated values in this Journal Entry.
+    #   defaults to true.
+    # @return [String] the catalog provided message for this Journal Entry, or
+    #   nil if non exists.
     def catalog(opts = {})
       return nil unless catalog?
 
@@ -64,10 +77,15 @@ module Systemd
       opts[:replace] ? field_substitute(cat) : cat
     end
 
+    # Returns true if this Journal Entry has an associated catalog message.
+    # @return [Boolean] whether or not this entry has an associated catalog
+    #   message.
     def catalog?
       !self[:message_id].nil?
     end
 
+    # Convert this Entry into a hash.
+    # @return [Hash] the hash representation of this journal entry.
     def to_h
       @entry.each_with_object({}) { |(k, v), h| h[k] = v.dup }
     end
@@ -79,6 +97,7 @@ module Systemd
 
     private
 
+    # @private
     def field_substitute(msg)
       msg.gsub(/@[A-Z_0-9]+@/) { |field| self[field[1..-2]] || field }
     end
