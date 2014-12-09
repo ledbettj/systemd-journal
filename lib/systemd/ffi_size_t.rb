539e1153c93ec0e34f508d4455a2d3baad7ed24f
John Ledbetter <john.ledbetter@gmail.com>
2014-12-09 15:32:08 -0500

    update FFI::MemoryPointer.{read,write}_size_t patch to work with jruby.
    Also JRuby FFI seems to be missing read_uint{32,64} so provide those as
    well.

--- lib/systemd/ffi_size_t.rb
+++ lib/systemd/ffi_size_t.rb
@@ -2,14 +2,23 @@ require 'ffi'
 
 # @private
 class FFI::MemoryPointer
-  # monkey patch a read_size_t and write_size_t method onto FFI::MemoryPointer
-  p = FFI::MemoryPointer.new(:size_t, 1)
-  w = case p.size
-      when 4 then :uint32
-      when 8 then :uint64
-      else raise RuntimeError.new("unsupported size_t width: #{p.size}")
-      end
+  # monkey patch a read_size_t and write_size_t method onto FFI::MemoryPointer.
+  # see https://github.com/ffi/ffi/issues/118
+  def self.monkey_patch_type_i_need!(which)
+    return if self.respond_to?("read_#{which}")
 
-  alias_method :read_size_t,  :"read_#{w}"  unless p.respond_to?(:read_size_t)
-  alias_method :write_size_t, :"write_#{w}" unless p.respond_to?(:write_size_t)
+    type = FFI.find_type(which)
+    type, _ = FFI::TypeDefs.find do |(name, t)|
+      method_defined?("read_#{name}") if t == type
+    end
+
+    raise RuntimeError, "Unable to patch in reader/writer for #{which}" if type.nil?
+
+    alias_method "read_#{which}", "read_#{type}"
+    alias_method "write_#{which}", "write_#{type}"
+  end
+end
+
+[:size_t, :uint64, :uint32].each do |type|
+  FFI::MemoryPointer.monkey_patch_type_i_need!(type)
 end
